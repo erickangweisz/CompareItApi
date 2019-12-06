e4ff1158fe3a5a2e7fd67443f7831f83b9e04a28
Pedro <pedrobusou@gmail.com>
2019-12-06 17:07:13 +0100

    Extract sprapProducts() from requestPromise()

--- src/scrapers/pccomponentes.js
+++ src/scrapers/pccomponentes.js
@@ -6,37 +6,34 @@ const baseUrl = 'https://www.pccomponentes.com';
 
 class Scraper {
   async getProducts(term) {
-    let products = [];
+    let products = [],
+      html = '';
 
     try {
-      products = await requestPromise(
-        `${baseUrl}/buscar/?query=${term}`,
-        (err, res, html) => {
-          if (!err && res.statusCode == 200) {
-            const $ = cheerio.load(html);
-            const products = [];
-
-            $('.tarjeta-articulo')
-              .slice(0, 2)
-              .each((i, productHTML) => {
-                const name = $(productHTML).data('name');
-                const price = $(productHTML).data('price');
-                const url = `${baseUrl}${$(productHTML)
-                  .find('.enlace-superpuesto')
-                  .attr('href')}`;
-
-                const product = new Product(name, price, url);
-                products.push(product);
-              });
-
-            console.log('Products:', products);
-            return products;
-          }
-        }
-      );
+      html = await requestPromise(`${baseUrl}/buscar/?query=${term}`);
+      products = this.scrapProducts(html);
     } catch (e) {
       console.error(e);
     }
+
+    return products;
+  }
+
+  scrapProducts(html) {
+    const products = [],
+      $ = cheerio.load(html);
+
+    $('.tarjeta-articulo')
+      .slice(0, 2) //TODO: take second parameter form searchParams (to be sended to the scraper)
+      .each((i, productHTML) => {
+        const name = $(productHTML).data('name');
+        const price = $(productHTML).data('price');
+        const url = `${baseUrl}${$(productHTML)
+          .find('.enlace-superpuesto')
+          .attr('href')}`;
+
+        products.push(new Product(name, price, url));
+      });
 
     return products;
   }
