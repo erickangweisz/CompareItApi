2d96bc39f82427590cf1c173382d5253ea50aff5
Pedro <pedrobusou@gmail.com>
2019-12-06 15:38:23 +0100

    Get only one item, to speed up req time. Format code

--- src/scrapers/pccomponentes.js
+++ src/scrapers/pccomponentes.js
@@ -1,38 +1,38 @@
-const requestPromise = require('request-promise')
-const cheerio = require('cheerio')
+const requestPromise = require('request-promise');
+const cheerio = require('cheerio');
 
 class Scraper {
-    async getProducts(term) {
-        let products = [];
-        
-        try {
-           const products = await requestPromise(
-                `https://www.pccomponentes.com/buscar/?query=${term}`, 
-                (err, res, html) => {
-                    if (!err && res.statusCode == 200) {
-                        const $ = cheerio.load(html);
-                        const products = [];
-        
-                        $('article').slice(0, 5).each((i, article) => {
-                            const art = $(article).text();
-                            console.log('Article', i);
-        
-                            //TODO: get correct elements
-                            //TODO: "new Product()" model should be used here
-                            console.log('art', art)
-                            products.push(art); 
-                        });
-                        
-                        return products;
-                    }
-                }
-            );
+  async getProducts(term) {
+    let products = [];
 
+    try {
+      products = await requestPromise(
+        `https://www.pccomponentes.com/buscar/?query=${term}`,
+        (err, res, html) => {
+          if (!err && res.statusCode == 200) {
+            const $ = cheerio.load(html);
+            const products = [];
+
+            $('article')
+              .slice(0, 1)
+              .each((i, article) => {
+                const art = $(article).text();
+                //TODO: get correct elements
+                //TODO: "new Product()" model should be used here
+                products.push(art);
+              });
+
+            console.error(products);
             return products;
-        } catch(e) {
-            console.error(e);
+          }
         }
+      );
+    } catch (e) {
+      console.error(e);
     }
+
+    return products;
+  }
 }
 
 module.exports = Scraper;
