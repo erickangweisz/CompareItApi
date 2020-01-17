0b0512eb603b18103fdbf260dfeda92a84c4bdbd
Pedro <pedrobusou@gmail.com>
2020-01-17 16:57:01 +0100

    Remove all scraper base code, leaving only code specific to this shop

--- src/scrapers/pccomponentes.js
+++ src/scrapers/pccomponentes.js
@@ -1,44 +1,15 @@
-const puppeteer = require('puppeteer');
-const Product = require('../models/product');
+/**
+ * Navigate to shop, perform search and scrapProducts
+ * @param page puppeteer browser.newPage() instance
+ * @param {string} searchTerm
+ * @param {number} nProducts
+ */
+async function scrapShop(page, searchTerm, nProducts) {
+  await page.goto('https://www.pccomponentes.com');
+  await page.type('.ais-SearchBox-input', searchTerm);
+  await page.waitForSelector('.ais-Hits-item');
 
-const domain = 'https://www.pccomponentes.com';
-
-class Scraper {
-  constructor(term, nProducts) {
-    this.term = term;
-    this.nProducts = +nProducts;
-  }
-
-  async getProducts() {
-    let products = [];
-
-    try {
-      const browser = await puppeteer.launch({
-        defaultViewport: {width: 1280, height: 800}
-      });
-      const page = await browser.newPage();
-      //page.on('console', consoleObj => console.log(consoleObj.text())); //Needed to show logs from inside page.evaluate() to the node console
-
-      await page.goto(domain);
-      await page.type('.ais-SearchBox-input', this.term);
-      await page.waitForSelector('.ais-Hits-item');
-
-      products = await page.evaluate(this.scrapProducts, this.nProducts);
-
-      await browser.close();
-    } catch (e) {
-      console.log(e);
-    }
-
-    return products;
-  }
-
-  /**
-   * Callback for page.evaluate()
-   * Se tendria que hacer una funcion de estas por cada tienda, ya que los valores a scrapear pueden estar en distintos sitios
-   * @param {number} nProducts
-   */
-  scrapProducts(nProducts) {
+  return await page.evaluate(nProducts => {
     const productElements = Array.from(
       document.querySelectorAll('.ais-Hits-item')
     ).slice(0, nProducts);
@@ -55,7 +26,7 @@ class Scraper {
         shopname: 'pccomponentes.com' //TODO: send imagotipo url instead
       };
     });
-  }
+  }, nProducts);
 }
 
-module.exports = Scraper;
+module.exports = scrapShop;
