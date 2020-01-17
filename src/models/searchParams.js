cb990d93209b51c2497c252e96769ffc4dee7b96
Pedro <pedrobusou@gmail.com>
2020-01-17 19:05:28 +0100

    Fix shops type. Format code.

--- src/models/searchParams.js
+++ src/models/searchParams.js
@@ -1,16 +1,15 @@
 class SearchParams {
-    /**
-     * SearchParams
-     * @param {string} term
-     * @param {string{}} shops
-     * @param {number} nProductsPerShop
-     */
-    constructor(term, shops, nProductsPerShop) {
-      this.term = term;
-      this.shops = shops;
-      this.nProductsPerShop = nProductsPerShop;
-    }
+  /**
+   * SearchParams
+   * @param {string} term
+   * @param {string[]} shops
+   * @param {number} nProductsPerShop
+   */
+  constructor(term, shops, nProductsPerShop) {
+    this.term = term;
+    this.shops = shops;
+    this.nProductsPerShop = nProductsPerShop;
   }
-  
-  module.exports = SearchParams;
-  
+}
+
+module.exports = SearchParams;
